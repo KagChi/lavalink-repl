33b16cd9daf0ff2cf19d6fc908b1ea738bfe5555
カグチ <59391215+KagChi@users.noreply.github.com>
2021-02-16 21:16:13 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash -s -- --skip-rc && . ~/.jabba/jabba.sh
 ## use java 13 :3
-jabba install openjdk@1.13.0-2
+jabba install openjdk@1.15.0
 npm i
 node index
