85549ddacec055ae9e6b2eab26674c89aa11cdad
KagChi <59391215+KagChi@users.noreply.github.com>
2022-06-24 10:28:03 +0700

    fix: could not download latest dev release

--- start.sh
+++ start.sh
@@ -1,12 +1,13 @@
 curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash -s -- --skip-rc && . ~/.jabba/jabba.sh
 jabba install zulu@1.13.0-2
 
+
 if [ ! -f "Lavalink.jar" ]; then
-    wget -O  Lavalink.jar https://ci.fredboat.com/repository/download/Lavalink_Build/.lastSuccessful/Lavalink.jar?guest=1&branch=refs/heads/dev
+    wget -O  Lavalink.jar "https://ci.fredboat.com/repository/download/Lavalink_Build/.lastSuccessful/Lavalink.jar?guest=1&branch=refs/heads/dev"
     java -jar Lavalink.jar
 else
   if [ "$AUTO_UPDATE" = "true" ]; then
-    wget -O  Lavalink.jar https://ci.fredboat.com/repository/download/Lavalink_Build/.lastSuccessful/Lavalink.jar?guest=1&branch=refs/heads/dev
+    wget -O  Lavalink.jar "https://ci.fredboat.com/repository/download/Lavalink_Build/.lastSuccessful/Lavalink.jar?guest=1&branch=refs/heads/dev"
     java -jar Lavalink.jar
   else
     java -jar Lavalink.jar
