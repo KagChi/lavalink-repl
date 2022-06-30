478f831106edc477e754988879d504a0d2be82b6
KagChi <59391215+KagChi@users.noreply.github.com>
2022-06-30 23:23:54 +0700

    fix: use curl to more consistency

--- start.sh
+++ start.sh
@@ -3,11 +3,11 @@ jabba install zulu@1.13.0-2
 
 
 if [ ! -f "Lavalink.jar" ]; then
-    wget -O  Lavalink.jar "https://ci.fredboat.com/repository/download/Lavalink_Build/.lastSuccessful/Lavalink.jar?guest=1&branch=refs/heads/dev"
+    curl --output  Lavalink.jar "https://ci.fredboat.com/repository/download/Lavalink_Build/.lastSuccessful/Lavalink.jar?guest=1&branch=refs/heads/dev"
     java -jar Lavalink.jar
 else
   if [ "$AUTO_UPDATE" = "true" ]; then
-    wget -O  Lavalink.jar "https://ci.fredboat.com/repository/download/Lavalink_Build/.lastSuccessful/Lavalink.jar?guest=1&branch=refs/heads/dev"
+    curl --output  Lavalink.jar "https://ci.fredboat.com/repository/download/Lavalink_Build/.lastSuccessful/Lavalink.jar?guest=1&branch=refs/heads/dev"
     java -jar Lavalink.jar
   else
     java -jar Lavalink.jar
